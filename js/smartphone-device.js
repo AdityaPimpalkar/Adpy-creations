8c9ddb26718b64a96927e648ce09da25bf2af89b
Aditya Pimpalkar <adipimpalkar14@gmail.com>
2018-01-29 23:34:16 +0530

    Update smartphone-device.js

--- js/smartphone-device.js
+++ js/smartphone-device.js
@@ -1,6 +1,10 @@
- function mediaSize() { 
+ function mediaSize() {
 if (window.matchMedia('(max-width : 480px)').matches) {
 	$(".replace-text").css({"display":"block"});
+	$(".img-wd").attr("src","images/develop-black.svg").css("width","120px");
+  $(".img-wde").attr("src","images/design-black.svg").css("width","120px");
+  $(".img-wds").attr("src","images/storage-black.svg").css("width","120px");
+  $(".img-wdse").attr("src","images/security-black.svg").css("width","120px");
 
 	//------WEB DEVELOPMENT---------//
 	$(".web-development").click(function(){
@@ -172,4 +176,101 @@ if (window.matchMedia('(max-width : 480px)').matches) {
 		$(".wds-inText").appendTo(".web-storage");
 		$(".wdse-inText").appendTo(".web-security");
 	}
+
+if (window.matchMedia('(min-width:482px) and (max-width: 770px)').matches) {
+	$(".replace-text").show();
+	$(".wd-back").hide();
+	$(".wd-fwd").show();
+	$(".wde-back").hide();
+	$(".wde-fwd").show();
+	$(".wds-back").hide();
+	$(".wds-fwd").show();
+	$(".wdse-back").hide();
+	$(".wdse-fwd").show();
+	$(".roc-fwd").show();
+	$(".biu-fwd").show();
+	$(".cr-fwd").show();
+	$(".sci-fwd").show();
+	function TabWebEx(e) {
+		e.stopPropagation();
+		$(".wdx-IMG").removeClass("wdx-IMG").addClass("wd-img");
+		$(".wdh-HD").removeClass("wdh-HD").addClass("wd-hd");
+		$(".wd-img,.wd-hd,.wd-fwd").hide();
+		$(".wd-back").show();
+		$(".web-development").off("click");
+	}
+		$(".web-development").on("click",TabWebEx);
+		$(".wd-back").on( "click",
+			function(e) {
+				e.stopPropagation();
+				$(".wd-img,.wd-hd,.wd-fwd").show();
+				$(".wd-back").hide();
+				$(".web-development").on("click",TabWebEx);
+			}
+		);
+
+	function TabDecEx(e) {
+		e.stopPropagation();
+		$(".wdex-IMG").removeClass("wdex-IMG").addClass("wde-img");
+		$(".wdex-HD").removeClass("wdex-HD").addClass("wde-hd");
+		$(".wde-img,.wde-hd,.wde-fwd").hide();
+		$(".wde-back").show();
+		$(".web-design").off("click");
+	}
+		$(".web-design").on("click",TabDecEx);
+		$(".wde-back").on( "click",
+			function(e) {
+				e.stopPropagation();
+				$(".wde-img,.wde-hd,.wde-fwd").show();
+				$(".wde-back").hide();
+				$(".web-design").on("click",TabDecEx);
+			}
+		);
+
+	function TabStoEx(e) {
+		e.stopPropagation();
+		$(".wdsx-IMG").removeClass("wdsx-IMG").addClass("wds-img");
+		$(".wdsx-HD").removeClass("wdsx-HD").addClass("wds-hd");
+		$(".wds-img,.wds-hd,.wds-fwd").hide();
+		$(".wds-back").show();
+		$(".web-storage").off("click");
+	}
+		$(".web-storage").on("click",TabStoEx);
+		$(".wds-back").on( "click",
+			function(e) {
+				e.stopPropagation();
+				$(".wds-img,.wds-hd,.wds-fwd").show();
+				$(".wds-back").hide();
+				$(".web-storage").on("click",TabStoEx);
+			}
+		);
+
+	function TabSecEx(e) {
+		e.stopPropagation();
+		$(".wdsxe-IMG").removeClass("wdsxe-IMG").addClass("wdse-img");
+		$(".wdsxe-HD").removeClass("wdsxe-HD").addClass("wdse-hd");
+		$(".wdse-img,.wdse-hd,.wdse-fwd").hide();
+		$(".wdse-back").show();
+		$(".web-security").off("click");
+	}
+		$(".web-security").on("click",TabSecEx);
+		$(".wdse-back").on( "click",
+			function(e) {
+				e.stopPropagation();
+				$(".wdse-img,.wdse-hd,.wdse-fwd").show();
+				$(".wdse-back").hide();
+				$(".web-security").on("click",TabSecEx);
+			}
+		);
+
+}else{
+	$(".wd-back").hide();
+	$(".wd-fwd").hide();
+	$(".wde-back").hide();
+	$(".wde-fwd").hide();
+	$(".roc-fwd").hide();
+	$(".biu-fwd").hide();
+	$(".cr-fwd").hide();
+	$(".sci-fwd").hide();
+}
 }
